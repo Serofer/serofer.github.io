b2eb28350af4279ceeb4228bcf6f119a6908429b
Serofer <vonwylamos@gmail.com>
2024-01-29 15:19:34 +0000

    reset and get repos

--- projects/script.js
+++ projects/script.js
@@ -1,5 +1,5 @@
 const username = 'Serofer';
-const maxPages = 1;
+const maxPages = 3;
 const hideForks = true;
 const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
